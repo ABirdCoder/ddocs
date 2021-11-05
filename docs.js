ab26184d38c57c5bd23bf6c28b34f08cc41cba75
ABirdCoder <82178308+BobTheBirdCodar@users.noreply.github.com>
2021-11-05 22:44:01 +0000

    Initial commit

---
+++ docs.js
@@ -0,0 +1,52 @@
+var heads = {};
+
+window.onload = () => {
+  const dheads = document.querySelectorAll("dhead");
+  const ul = document.createElement("ul");
+  ul.classList.add("docslink");
+
+  dheads.forEach(item => {
+    item.id = 'ddocs' + item.innerHTML;
+    
+    
+    const li = document.createElement("li");
+    const h3 = document.createElement("h3");
+    const a = document.createElement("a");
+
+    li.appendChild(h3);
+    h3.appendChild(a);
+
+    a.href = "#ddocs" + item.innerHTML;
+    a.innerHTML = item.innerHTML;
+
+    ul.appendChild(li);
+
+    heads[a.innerHTML] = a;
+  });
+
+  
+  document.body.appendChild(ul);
+
+}
+
+window.onscroll = () => {
+  const dheads = document.querySelectorAll("dhead"); 
+
+  var found = false;
+  var whatfound = undefined;
+  dheads.forEach(item => {
+    const rect = item.getBoundingClientRect();
+
+    if (window.scrollY >= rect.top && window.scrollY <= rect.bottom && !found) {
+      heads[item.innerHTML].classList.add("dselected");
+      found = true;
+      whatfound = item;
+    }
+  });
+
+  dheads.forEach(item => {
+    if (found && item != whatfound) {
+      heads[item.innerHTML].classList.remove("dselected");
+    }
+  })
+}
