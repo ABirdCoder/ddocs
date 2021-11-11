a83c9bc6e7992b8d2817e0c0e0fcca3e0469bfe5
ABirdCoder <82178308+BobTheBirdCodar@users.noreply.github.com>
2021-11-11 00:52:49 +0000

    Dark theme + Better CSS

--- docs.js
+++ docs.js
@@ -1,4 +1,5 @@
 var heads = {};
+const root = document.documentElement;
 
 window.onerror = (errormsg, loc, line) => {
   alert(errormsg + " at line: " + line);
@@ -8,8 +9,71 @@ window.onload = () => {
 
   const dheads = document.querySelectorAll("dhead");
   const dcode = document.querySelectorAll("dcode");
+  const div = document.createElement("div");
   const ul = document.createElement("ul");
-  ul.classList.add("docslink");
+  div.classList.add("docslink");
+  div.appendChild(ul);
+
+  const button = document.createElement("button");
+
+  button.innerHTML = "&#9790";
+  button.classList.add("light-mode")
+  div.appendChild(button);
+
+  button.addEventListener("click", () => {
+    if (button.classList.contains("light-mode")) {
+      button.classList.remove("light-mode");
+      button.classList.add("dark-mode");
+    }
+    else {
+      button.classList.remove("dark-mode");
+      button.classList.add("light-mode");
+    }
+    if (button.classList.contains("dark-mode")) {
+      button.innerHTML = "&#9728";
+
+
+      root.style.setProperty("--docs-bg",  "#1b1b1b");
+      root.style.setProperty("--code-bg",  "#444444");
+      root.style.setProperty("--sample-color", "darkred");
+      root.style.setProperty("--info-color",  "rgb(199, 199, 199)");
+      root.style.setProperty("--base-color",  "white");
+
+      root.style.setProperty("--bookmark-unselected",  "rgb(134, 134, 134)");
+      root.style.setProperty("--bookmark-background",  "#111111");
+      root.style.setProperty("--bookmark-selected",  "#36a4cf");
+
+      root.style.setProperty("--scrollbar-code-color",  "rgb(63, 63, 63)");
+      root.style.setProperty("--scrollbar-code-color-hover",  "rgb(59, 59, 59)");
+
+      root.style.setProperty("--copy-button-color",  "rgb(255, 255, 255)");
+      root.style.setProperty("--copy-button-bg",  "#111a42");
+
+      document.getElementById("dark").removeAttribute("disabled");
+    }
+    else {
+      button.innerHTML = "&#9790";
+
+
+      root.style.setProperty("--docs-bg",  "white");
+      root.style.setProperty("--code-bg",  "#f1f1f1");
+      root.style.setProperty("--sample-color", "crimson");
+      root.style.setProperty("--info-color",  "grey");
+      root.style.setProperty("--base-color",  "black");
+
+      root.style.setProperty("--bookmark-unselected",  "rgb(70, 68, 68)");
+      root.style.setProperty("--bookmark-background",  "#ebebeb");
+      root.style.setProperty("--bookmark-selected",  "#36a4cf");
+
+      root.style.setProperty("--scrollbar-code-color",  "#888");
+      root.style.setProperty("--scrollbar-code-color-hover",  "#555");
+
+      root.style.setProperty("--copy-button-color",  "white");
+      root.style.setProperty("--copy-button-bg",  "#3aa0ff");
+
+      document.getElementById("dark").setAttribute("disabled", "disabled");
+    }
+  })
 
   dheads.forEach(item => {
     item.id = 'ddocs' + item.innerText;
@@ -22,8 +86,10 @@ window.onload = () => {
     li.appendChild(h3);
     h3.appendChild(a);
 
-    a.href = "#ddocs" + item.innerText;
-    a.innerHTML = item.innerText;
+    const maxLength = 13;
+
+    a.href = item.innerText.length > maxLength ? "#ddocs" + item.innerText : "#ddocs" + item.innerText;
+    a.innerHTML = item.innerText.length > maxLength ?item.innerText.substr(0, maxLength) + "..." :  item.innerText.substr(0, maxLength);
 
     ul.appendChild(li);
 
@@ -31,11 +97,11 @@ window.onload = () => {
       a.classList.add("dselected")
     }
 
-    heads[a.innerHTML] = a;
+    heads[a.innerText] = [a, item.innerText];
   });
 
 
-  document.body.appendChild(ul);
+  document.body.appendChild(div);
 
 
   dcode.forEach(item => {
@@ -52,7 +118,7 @@ window.onload = () => {
       }, function (err) {
         item.innerText = "Failed to copy!"
       });
-      setTimeout(function() {
+      setTimeout(function () {
         item.innerHTML = "Copy &#x2398;"
       }, 1000);
     })
@@ -68,9 +134,9 @@ window.onscroll = () => {
   var whatfound = undefined;
   dheads.forEach(item => {
     const rect = item.getBoundingClientRect();
-    
+
     if (window.scrollY >= rect.top && window.scrollY <= rect.bottom && !found) {
-      heads[item.innerText].classList.add("dselected");
+      heads[htmlEntities(item[1].innerText)].classList.add("dselected");
       found = true;
       whatfound = item;
     }
@@ -78,7 +144,12 @@ window.onscroll = () => {
 
   dheads.forEach(item => {
     if (found && item != whatfound) {
-      heads[item.innerText].classList.remove("dselected");
+
+      heads[htmlEntities(item[1].innerText)].classList.remove("dselected");
     }
   })
+}
+
+function htmlEntities(str) {
+  return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
